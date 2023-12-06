4184b18a0682f85e0e568646183e5cc4795c7b84
superlutpi <girdza248@gmail.com>
2023-12-06 23:46:50 +0700

    mulai ulang lagi wleee

---
+++ app/src/main/java/com/lutpi/capstone/ui/preview/PreviewActivity.kt
@@ -0,0 +1,12 @@
+package com.lutpi.capstone.ui.preview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lutpi.capstone.R
+
+class PreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
+}
